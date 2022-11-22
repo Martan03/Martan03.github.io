c854b7ee7a61eec96a143ec8208ff8784531126d
Martan03 <martinslezak03@gmail.com>
2022-11-22 20:55:34 +0100

    Add send email function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const submitButton = document.querySelector('.contact-submit');
-
-if (submitButton)
+function sendEmail()
 {
-    
+    const message = document.querySelector('.message').value;
+    const email = document.querySelector('.email').value;
+    window.open('mailto:martinslezak03@gmail.com?subject=Web: ' + email + '&body=' + message);
 }
